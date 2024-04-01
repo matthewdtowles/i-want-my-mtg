d9fcbd2d2df6a224f34cffeb88fadb0d3beb8b28
matthewdtowles <matthewdtowles@gmail.com>
2024-04-01 16:22:59 -0400

    support npm run start

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 const SOURCE_URL = 'https://mtgjson.com/api/v5/';
 
-async function fetchCardsFromSet(name: string) {
+async function fetchSetJson(name: string) {
     try {
         const response: AxiosResponse<any> = await axios.get(SOURCE_URL + name + '.json');
         return response.data;
@@ -12,7 +12,7 @@ async function fetchCardsFromSet(name: string) {
     }
 }
 
-fetchCardsFromSet('KLD').then((data) => {
+fetchSetJson('KLD').then((data) => {
     if (data) {
         console.log(data);
     }

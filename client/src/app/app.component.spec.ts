ec9e49eca1abadfedd3df1cad44f05e549b7b459
matthewdtowles <matthewdtowles@gmail.com>
2024-04-14 19:11:02 -0400

    restructure app into 3 subprojects

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-mtg-angular' title`, () => {
+  it(`should have the 'client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-mtg-angular');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-mtg-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
   });
 });

0c70defe84c084bfa42931f4b2994a81c2cf118c
matthewdtowles <matthewdtowles@gmail.com>
2024-07-07 22:13:28 -0400

    intf init impl; impl data ingest

---
+++ src/api/app-service.interface.ts
@@ -0,0 +1,4 @@
+// TODO: superfluous?
+export interface IAppService {
+    
+}
--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SetService } from './set/set.service';
+import { SetService } from '../set/set.service';
 
 describe('AppController', () => {
   let appController: AppController;

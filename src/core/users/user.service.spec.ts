29da7034f01c08a94201eba12de199e63c5c1c4f
Matthew Towles <matthewdtowles@gmail.com>
2024-08-01 23:03:24 -0400

    User module impl (#1)
    * user service spec fixed

--- src/core/users/user.service.spec.ts
+++ src/core/users/user.service.spec.ts
@@ -1,19 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserRepository } from './ports/user.repository';
 
-const mockUser = {
-    id: 1,
-    email: 'test-email1@iwantmymtg.com',
-    username: 'test-username1',
-    password: 'test-password1'
+const mockUser: User = new User();
+mockUser.id = 1;
+mockUser.email = 'test-email1@iwantmymtg.com';
+mockUser.username = 'test-username1';
+mockUser.password = 'test-password1';
+    
+const mockUserRepository = {
+    saveUser: jest.fn().mockResolvedValue(mockUser),
+    userExists: jest.fn().mockResolvedValue(false),
+    findById: jest.fn().mockResolvedValue(mockUser),
+    findByUsername: jest.fn().mockResolvedValue(mockUser),
+    removeById: jest.fn(),
+    removeUser: jest.fn(),
+    delete: jest.fn(),
+    findOneBy: jest.fn().mockResolvedValue(mockUser),
 };
 
 describe('UserService', () => {
     let service: UserService;
-    let repository: Repository<User>;
+    let repository: typeof mockUserRepository;
+
     const user: User = new User();
     user.email = 'test-email1@iwantmymtg.com';
     user.username = 'test-username1';
@@ -24,58 +34,42 @@ describe('UserService', () => {
             providers: [
                 UserService,
                 {
-                    provide: getRepositoryToken(User),
-                    useValue: {
-                        findById: jest.fn().mockResolvedValue(mockUser),
-                        findByUsername: jest.fn().mockResolvedValue(mockUser),
-                        findOneBy: jest.fn().mockResolvedValue(mockUser),
-                        save: jest.fn().mockResolvedValue(mockUser),
-                        remove: jest.fn(),
-                        delete: jest.fn(),
-                    },
+                    provide: UserRepository,
+                    useValue: mockUserRepository,
                 },
             ],
         }).compile();
 
         service = module.get<UserService>(UserService);
-        repository = module.get<Repository<User>>(getRepositoryToken(User));
+        repository = module.get(UserRepository);
     });
 
     it('users service should be defined', () => {
         expect(service).toBeDefined();
     });
 
-    describe('users service create()', () => {
-        it('should successfully insert a user', () => {
-
-            expect(
-                service.create(user),
-            ).resolves.toEqual(mockUser);
-        });
+    it('create should successfully insert a user', () => {
+        expect(service.create(user)).resolves.toEqual(mockUser);
     });
 
-    describe('users service findById()', () => {
-        it('should get a single user with passed id', () => {
-            const repoSpy = jest.spyOn(repository, 'findOneBy');
-            expect(service.findById(1)).resolves.toEqual(mockUser);
-            expect(repoSpy).toHaveBeenCalledWith({ id: 1 });
-        });
+    it('findById should get a single user with passed id', () => {
+        const repoSpy = jest.spyOn(repository, 'findById');
+        expect(service.findById(1)).resolves.toEqual(mockUser);
+        expect(repoSpy).toHaveBeenCalledWith(1);
     });
 
-    describe('users service findByUsername()', () => {
-        it('should get a single user with passed username', () => {
-            const repoSpy = jest.spyOn(repository, 'findOneBy');
-            expect(service.findByUsername('test-username1')).resolves.toEqual(mockUser);
-            expect(repoSpy).toHaveBeenCalledWith({ username: 'test-username1' });
-        });
+    it('findByUsername should get a single user with passed username', () => {
+        const repoSpy = jest.spyOn(repository, 'findByUsername');
+        expect(service.findByUsername('test-username1')).resolves.toEqual(mockUser);
+        expect(repoSpy).toHaveBeenCalledWith('test-username1');
     });
 
-    describe('users service remove()', () => {
-        it('should call remove with the passed id', async () => {
-            const removeSpy = jest.spyOn(repository, 'delete');
-            const retVal = await service.remove(user);
-            expect(removeSpy).toHaveBeenCalledWith(1);
-            expect(retVal).toBeUndefined();
-        });
+    it('remove should delete given user, check if user exists and return false', async () => {
+        const removeSpy = jest.spyOn(repository, 'removeUser');
+        const existsSpy = jest.spyOn(repository, 'userExists');
+        const retVal = await service.remove(user);
+        expect(removeSpy).toHaveBeenCalledWith(user);
+        expect(existsSpy).toHaveBeenCalledWith(user);
+        expect(retVal).toBe(false);
     });
 });

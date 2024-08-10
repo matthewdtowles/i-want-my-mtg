96faaa2125cbe55d1c6cef95410b29f8abd5fce3
matthewdtowles <matthewdtowles@gmail.com>
2024-08-10 00:12:34 -0400

    test utils for mock card, set

--- test/core/card/card.service.spec.ts
+++ test/core/card/card.service.spec.ts
@@ -3,32 +3,36 @@ import { CardService } from '../../../src/core/card/card.service';
 import { CardRepositoryPort } from '../../../src/core/card/ports/card.repository.port';
 import { CardDataIngestionPort } from '../../../src/core/card/ports/card-data.ingestion.port';
 import { Card } from '../../../src/core/card/card';
+import { TestUtils } from '../../test-utils';
 
-let createdCard: Card = new Card();
-const mockSetCards: Card[] = [];
-const mockCardsWithName: Card[] = [];
-const mockCard: Card = new Card();
-
-const mockCardRepository: CardRepositoryPort = {
-    cardExists: jest.fn().mockResolvedValue(false),
-    findAllInSet: jest.fn().mockResolvedValue(mockSetCards),
-    findAllWithName: jest.fn().mockResolvedValue(mockCardsWithName),
-    findById: jest.fn().mockResolvedValue(mockCard),
-    findBySetCodeAndNumber: jest.fn().mockResolvedValue(mockCard),
-    findByUuid: jest.fn().mockResolvedValue(mockCard),
-    removeById: jest.fn(),
-    saveCard: jest.fn().mockResolvedValue(createdCard),
-};
-
-const mockCardIngestion: CardDataIngestionPort = {
-    fetchCard: jest.fn(),
-    fetchSetCards: jest.fn(),
-};
 
 describe('CardService', () => {
+    const testUtils: TestUtils = new TestUtils();
     let service: CardService;
     let repository: CardRepositoryPort;
     let ingestionSvc: CardDataIngestionPort;
+    let mockSetCode: string = 'SET';
+
+    let createdCard: Card = new Card();
+    const mockSetCards: Card[] = testUtils.getMockSetCards(mockSetCode);
+    const mockCardsWithName: Card[] = testUtils.getMockCardsWithName(3);
+    const mockCard: Card = testUtils.getMockCard(1, mockSetCode);
+
+    const mockCardRepository: CardRepositoryPort = {
+        cardExists: jest.fn().mockResolvedValue(false),
+        findAllInSet: jest.fn().mockResolvedValue(mockSetCards),
+        findAllWithName: jest.fn().mockResolvedValue(mockCardsWithName),
+        findById: jest.fn().mockResolvedValue(mockCard),
+        findBySetCodeAndNumber: jest.fn().mockResolvedValue(mockCard),
+        findByUuid: jest.fn().mockResolvedValue(mockCard),
+        removeById: jest.fn(),
+        saveCard: jest.fn().mockResolvedValue(createdCard),
+    };
+
+    const mockCardIngestion: CardDataIngestionPort = {
+        fetchCard: jest.fn(),
+        fetchSetCards: jest.fn(),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -37,7 +41,7 @@ describe('CardService', () => {
                 {
                     provide: CardRepositoryPort,
                     useValue: mockCardRepository,
-                }, 
+                },
                 {
                     provide: CardDataIngestionPort,
                     useValue: mockCardIngestion,
@@ -54,15 +58,15 @@ describe('CardService', () => {
         expect(service).toBeDefined();
     });
 
-    it('creates and returns persisted instance of given card', () => {});
+    it('creates and returns persisted instance of given card', () => { });
 
-    it('finds all cards in given set by setCode', () => {});
+    it('finds all cards in given set by setCode', () => { });
 
-    it('finds every instance of a card with given name', () => {});
+    it('finds every instance of a card with given name', () => { });
 
-    it('finds a unique instance of a card by uuid', () => {});
+    it('finds a unique instance of a card by uuid', () => { });
 
-    it('finds a unique instance of a card by setCode and card number in that set', () => {});
+    it('finds a unique instance of a card by setCode and card number in that set', () => { });
 
-    it('updates and returns persisted instance of given card', () => {});
+    it('updates and returns persisted instance of given card', () => { });
 });
--- test/test-utils.ts
+++ test/test-utils.ts
@@ -1,3 +1,71 @@
+import { Set } from '../src/core/set/set';
+import { Card } from '../src/core/card/card';
+
 export class TestUtils {
-    
+
+    private readonly MOCK_BASE_SET_SIZE: number = 3;
+    private readonly MOCK_SET_NAME: string = 'Setname';
+    private readonly MOCK_RELEASE_DATE: string = '1970-01-01';
+    private readonly MOCK_SET_TYPE: string = 'expansion';
+    private readonly MOCK_SET_URL: string = 'sets/set';
+    private readonly MOCK_ROOT_SCRYFALL_ID: string = 'abc123def456';
+    private readonly IMG_SRC_BASE: string = 'https://cards.scryfall.io/normal/front/';
+
+    getMockSetCards(setCode: string): Card[] {
+        const cards: Card[] = [];
+        for (let i = 1; i <= this.MOCK_BASE_SET_SIZE; i++) {
+            cards.push(this.getMockCard(i, setCode));
+        }
+        let bonusCard = new Card();
+        bonusCard.imgSrc = this.IMG_SRC_BASE + '4/a/4' + this.MOCK_ROOT_SCRYFALL_ID + '.jpg';
+        bonusCard.isReserved = false;
+        bonusCard.manaCost = '{U/G}{B/W}{R/U}';
+        bonusCard.name = 'Test Bonus Card Name';
+        bonusCard.number = (this.MOCK_BASE_SET_SIZE + 1).toString();
+        bonusCard.originalText = 'Bonus card text.';
+        bonusCard.rarity = 'mythic';
+
+        bonusCard.set = this.getMockSet(setCode);
+        bonusCard.url = this.MOCK_SET_URL + '/' + bonusCard.number;
+        bonusCard.uuid = 'zyxw-0987-vutsr-6543-qponm-21098';
+        cards.push(bonusCard);
+        return cards;
+    }
+
+    getMockSet(setCode: string): Set {
+        const set: Set = new Set();
+        set.baseSize = this.MOCK_BASE_SET_SIZE;
+        set.block = this.MOCK_SET_NAME;
+        set.keyruneCode = setCode.toLowerCase();
+        set.name = this.MOCK_SET_NAME;
+        set.releaseDate = this.MOCK_RELEASE_DATE;
+        set.setCode = setCode;
+        set.type = this.MOCK_SET_TYPE;
+        return set;
+    }
+
+    getMockCardsWithName(total: number): Card[] {
+        const cards: Card[] = [];
+        for (let i = 0; i < total; i++) {
+            const s: string = i.toString();
+            cards.push(this.getMockCard(i, s + s + s));
+        }
+        return cards;
+    }
+
+    getMockCard(setNumber: number, setCode: string): Card {
+        let card = new Card();
+        card.imgSrc = this.IMG_SRC_BASE + setNumber + '/' + 'a/' + setNumber + this.MOCK_ROOT_SCRYFALL_ID + '.jpg';
+        card.isReserved = false;
+        card.manaCost = '{' + setNumber + '}{W}';
+        card.name = 'Test Card Name' + setNumber;
+        card.number = setNumber.toString();
+        card.originalText = 'Test card text.';
+        card.uuid = 'abcd-1234-efgh-5678-ijkl-' + setCode + setNumber;
+        card.rarity = setNumber % 2 === 1 ? 'common' : 'uncommon';
+        card.set = this.getMockSet(setCode);
+        card.url = this.MOCK_SET_URL + '/' + setNumber;
+        return card;
+    }
+
 }

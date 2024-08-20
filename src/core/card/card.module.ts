a5af2367d54dfe9397276408b88bbca09954daad
matthewdtowles <matthewdtowles@gmail.com>
2024-08-20 18:48:56 -0400

    * * Card and Set Modules Streamlined * *

--- src/core/card/card.module.ts
+++ src/core/card/card.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { CardRepository } from 'src/adapters/database/card.repository';
 import { DatabaseModule } from 'src/adapters/database/database.module';
 import { MtgJsonIngestionModule } from 'src/adapters/mtgjson-ingestion/mtgjson-ingestion.module';
-import { MtgJsonIngestionService } from 'src/adapters/mtgjson-ingestion/mtgjson-ingestion.service';
-import { IngestionServicePort } from '../ingestion/ingestion.service.port';
 import { CardService } from './card.service';
 import { CardRepositoryPort } from './ports/card.repository.port';
 import { CardServicePort } from './ports/card.service.port';
@@ -19,18 +17,11 @@ import { CardServicePort } from './ports/card.service.port';
             useClass: CardService,
         },
         {
-            provide: IngestionServicePort,
-            useClass: MtgJsonIngestionService,
-        },
-        {
             provide: CardRepositoryPort,    
             useClass: CardRepository,
         },
-        // MtgJsonApiClient
     ],
     exports: [
-        IngestionServicePort,
-        CardRepositoryPort,
         CardServicePort,
     ]
 })
--- src/core/set/set.module.ts
+++ src/core/set/set.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { SetRepository } from 'src//adapters/database/set.repository';
 import { DatabaseModule } from 'src/adapters/database/database.module';
 import { MtgJsonIngestionModule } from 'src/adapters/mtgjson-ingestion/mtgjson-ingestion.module';
-import { MtgJsonIngestionService } from 'src/adapters/mtgjson-ingestion/mtgjson-ingestion.service';
-import { IngestionServicePort } from '../ingestion/ingestion.service.port';
 import { SetRepositoryPort } from './ports/set.repository.port';
 import { SetServicePort } from './ports/set.service.port';
 import { SetService } from './set.service';
@@ -19,18 +17,16 @@ import { SetService } from './set.service';
             useClass: SetService,
         },
         {
-            provide: IngestionServicePort,
-            useClass: MtgJsonIngestionService,
-        },
-        {
             provide: SetRepositoryPort,    
             useClass: SetRepository,
         },
     ],
     exports: [
-        IngestionServicePort,
-        SetRepositoryPort,
         SetServicePort,
     ]
 })
-export class SetModule { }
+export class SetModule {
+    constructor() {
+        console.log('* * SetModule Initialized * *');
+    }
+}

a2be32a3fe8ad7dfa2fd6b02af84b085fd7d0ec9
matthewdtowles <matthewdtowles@gmail.com>
2024-08-25 21:17:15 -0400

    app fixed and running

--- src/core/user/user.entity.ts
+++ src/core/user/user.entity.ts
@@ -1,5 +1,5 @@
 import { Collection } from 'src/core/collection/collection.entity';
-import { Column, Entity, Index, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -14,6 +14,5 @@ export class User {
     name: string;
 
     @OneToOne(() => Collection, collection => collection.owner)
-    @Index()
     collection: Collection;
 }

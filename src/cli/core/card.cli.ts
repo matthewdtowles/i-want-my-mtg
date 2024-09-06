07fd8c84ee7366c930d85028d58d7809fdb4635f
matthewdtowles <matthewdtowles@gmail.com>
2024-09-06 19:00:10 -0400

    card cli impl

--- src/cli/core/card.cli.ts
+++ src/cli/core/card.cli.ts
@@ -12,6 +12,7 @@ export class CardCli {
         @Inject(CardServicePort) private readonly service: CardServicePort
     ) { }
 
+
     @Command({
         command: 'get-card <code> <setNumber>',
         describe: 'Retrieve card with set number from set with given code'
@@ -26,16 +27,50 @@ export class CardCli {
             name: 'setNumber',
             describe: 'the set number of the card',
             type: 'number'
-        }) setNumber: number
-    ): Promise<CardDto | null> {
-        return this.service.findBySetCodeAndNumber(code, setNumber);
+        }) setNumber: number): Promise<void> {
+        const card: CardDto = await this.service.findBySetCodeAndNumber(code, setNumber);
+        this.LOGGER.log(this.formatOutput(card));
     }
+
+
+    @Command({
+        command: 'cards-in-set <code>',
+        describe: 'Retrieve all cards in set with given code'
+    })
+    async getSetCards(@Positional({
+        name: 'code',
+        describe: 'the set code from which all cards are returned',
+        type: 'string'
+    }) code: string): Promise<void> {
+        const cards: CardDto[] = await this.service.findAllInSet(code);
+        this.LOGGER.log(this.formatOutput(cards));
+    }
+
+
+    @Command({
+        command: 'cards-by-name <name>',
+        describe: 'Retrieve all cards with given name'
+    })
+    async getCardsWithName(@Positional({
+        name: 'name',
+        describe: 'the name of the cards to retrieve',
+        type: 'string'
+    }) name: string): Promise<void> {
+        const cards: CardDto[] = await this.service.findAllWithName(name);
+        this.LOGGER.log(this.formatOutput(cards));
+    }
+
 
     @Command({
         command: 'card-test',
         describe: 'Test Card CLI',
     })
     async cardCliTest(): Promise<void> {
-        this.LOGGER.debug(`Card CLI invoked`);
+        this.LOGGER.log(`Card CLI invoked`);
+    }
+
+
+    private formatOutput(cards: CardDto[] | CardDto): string {
+        return `\n${JSON.stringify(cards, null, 4)}`;
     }
 }

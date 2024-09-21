ccd2f9f4f8c018e1e88edf3b63f98f64a4e2d547
matthewdtowles <matthewdtowles@gmail.com>
2024-09-21 15:02:23 -0400

    create user page working

--- src/adapters/http/user.controller.ts
+++ src/adapters/http/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Redirect, Render } from '@nestjs/common';
 import { UpdateUserDto } from 'src/core/user/dto/update-user.dto';
 import { UserDto } from 'src/core/user/dto/user.dto';
 import { UserServicePort } from 'src/core/user/ports/user.service.port';
@@ -8,34 +8,44 @@ import { CreateUserDto } from '../../core/user/dto/create-user.dto';
 export class UserController {
     constructor(@Inject(UserServicePort) private readonly userService: UserServicePort) { }
 
-    @Post('register')
-    async create(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
-        return this.userService.create(createUserDto);
+    @Get('create')
+    @Render('create-user')
+    createForm() {
+        return {};
+    }
+
+    @Post('create')
+    @Redirect()
+    async create(@Body() createUserDto: CreateUserDto) {
+        const createdUser: UserDto = await this.userService.create(createUserDto);
+        return { url: `/user/${createdUser.id}` };
     }
 
     @Get(':id')
     @Render('user')
     async findById(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
-        return this.userService.findById(id);
-    }
-
-    @Get('email/:email')
-    @Render('user')
-    async findByEmail(@Param('email') email: string): Promise<UserDto> {
-        return this.userService.findByEmail(email);
+        return await this.userService.findById(id);
     }
 
     @Patch('update')
     @Render('user')
     async update(@Body() updateUserDto: UpdateUserDto): Promise<UserDto> {
-        console.log(`updateUserDto: ${JSON.stringify(updateUserDto)}`);
-        return this.userService.update(updateUserDto);
+        return await this.userService.update(updateUserDto);
     }
 
     // TODO: use just the ID?
-    @Delete(':user')
-    @Render('deleted-user') // TODO: or have logic for this in user.hbs?
-    async remove(@Body('user') user: UpdateUserDto): Promise<void> {
-        return this.userService.remove(user);
+    @Delete('delete/:id')
+    @Render('delete-user') // TODO: or have logic for this in user.hbs?
+    async remove(@Body('user') user: UpdateUserDto): Promise<string> {
+        const limit: number = 3;
+        let i = 0;
+        do {
+            i++;
+            await this.userService.remove(user);
+        } while(await this.userService.findById(user.id) && i < limit);
+        const msgPrefix: string = `User ${user.name}`;
+        const successMsg: string = `${msgPrefix} has been successfully removed.`;
+        const failMsg: string = `${msgPrefix} could not be removed.`;
+        return i < limit ? successMsg : failMsg;
     }
 }

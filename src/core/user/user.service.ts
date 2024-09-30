fa2271f73cfa5ff6fefd29a68727648d1f5e0f1f
matthewdtowles <matthewdtowles@gmail.com>
2024-09-30 18:54:59 -0400

    encrypt pwd on save

--- src/core/user/user.service.ts
+++ src/core/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { plainToInstance } from 'class-transformer';
 import { User } from 'src/core/user/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,6 +7,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UserRepositoryPort } from './ports/user.repository.port';
 import { UserServicePort } from './ports/user.service.port';
+
 
 @Injectable()
 export class UserService implements UserServicePort {
@@ -17,7 +19,7 @@ export class UserService implements UserServicePort {
 
     async create(userDto: CreateUserDto): Promise<UserDto> {
         const user: User = plainToInstance(User, userDto);
-        user.password = userDto.password;
+        user.password = await this.encrypt(userDto.password);
         const savedUser: User = await this.repository.create(user) ?? new User();
         return plainToInstance(UserDto, savedUser);
     }
@@ -37,9 +39,14 @@ export class UserService implements UserServicePort {
         user.password = userDto.password;
         const savedUser: User = await this.repository.update(user) ?? new User();
         return plainToInstance(UserDto, savedUser);
-    }
+}
 
     async remove(id: number): Promise<void> {
         await this.repository.delete(id);
+    }
+
+    private async encrypt(password: string): Promise<string> {
+        const saltRounds = 10; // TODO: check best practices
+        return await bcrypt.hash(password, saltRounds);
     }
 }

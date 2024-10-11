85da5021a4b0bfbd05561edf632afb3ce10b1cd5
matthewdtowles <matthewdtowles@gmail.com>
2024-10-10 22:24:56 -0400

    Error Exception Filter Error Page

---
+++ src/adapters/http/http.exception.filter.ts
@@ -0,0 +1,19 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Request, Response } from "express";
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const _status: number = exception.getStatus ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        response.
+            status(_status).
+            render('error', {
+                status: _status,
+                message: exception.message || 'Internal Server Error',
+            });
+    }
+}

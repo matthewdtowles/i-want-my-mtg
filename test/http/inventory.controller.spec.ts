bc2ab7ea31df656160b3ef807b1045dba757df4c
matthewdtowles <matthewdtowles@gmail.com>
2024-11-08 09:49:11 -0500

    test fixed - all working

--- test/http/inventory.controller.spec.ts
+++ test/http/inventory.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { InventoryController } from "src/adapters/http/inventory.controller";
+import { AggregatorServicePort } from "src/core/aggregator/api/aggregator.service.port";
 import { InventoryServicePort } from "src/core/inventory/api/inventory.service.port";
 
 describe("InventoryController", () => {
@@ -10,8 +11,12 @@ describe("InventoryController", () => {
             providers: [
                 {
                     provide: InventoryServicePort,
-                    useValue: {},
+                    useValue: {}
                 },
+                {
+                    provide: AggregatorServicePort,
+                    useValue: {}
+                }
             ],
         }).compile();
         controller = module.get<InventoryController>(InventoryController);

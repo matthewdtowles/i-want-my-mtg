583aad748577c257920a7bab602cc117cef70430
matthewdtowles <matthewdtowles@gmail.com>
2024-11-15 23:53:11 -0500

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{html,ts,js,hbs}',
+        './views/**/*.{hbs,html}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+

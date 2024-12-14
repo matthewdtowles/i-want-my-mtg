c7d85921d497c744c0c4b986b52f783c8ca4fd34
matthewdtowles <matthewdtowles@gmail.com>
2024-12-14 17:17:04 -0500

    fix card breadcrumbs, inventory null checks

--- src/adapters/http/card.controller.ts
+++ src/adapters/http/card.controller.ts
@@ -55,9 +55,10 @@ export class CardController {
         return {
             authenticated: req.isAuthenticated(),
             breadcrumbs: [
-                {label: "Home", url: "/"},
-                {label: _card.setCode.toUpperCase(), url: `/sets/${_card.setCode}`},
-                {label: _card.name, url: `/card/${_card.setCode}/${_card.number}`},
+                { label: "Home", url: "/" },
+                { label: "Sets", url: "/sets" },
+                { label: _card.setCode.toUpperCase(), url: `/sets/${_card.setCode}` },
+                { label: _card.name, url: `/card/${_card.setCode}/${_card.number}` },
             ],
             card: _card,
             message: HttpStatus.OK ? "Card found" : "Card not found",
@@ -88,9 +89,10 @@ export class CardController {
         return {
             authenticated: req.isAuthenticated(),
             breadcrumbs: [
-                {label: "Home", url: "/"},
-                {label: setCode.toUpperCase(), url: `/sets/${setCode}`},
-                {label: _card.name, url: `/card/${setCode}/${setNumber}`},
+                { label: "Home", url: "/" },
+                { label: "Sets", url: "/sets" },
+                { label: setCode.toUpperCase(), url: `/sets/${setCode}` },
+                { label: _card.name, url: `/card/${setCode}/${setNumber}` },
             ],
             card: _card,
             message: HttpStatus.OK ? "Card found" : "Card not found",

eb10db091e5838b8cfa583aea7530b191de80800
matthewdtowles <matthewdtowles@gmail.com>
2025-06-15 02:17:33 -0400

    create user dto file

---
+++ src/adapters/http/user/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString, IsStrongPassword, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()
+    readonly email: string;
+
+    @IsString()
+    @MinLength(6)
+    readonly name: string;
+
+    @IsStrongPassword()
+    readonly password: string;
+}

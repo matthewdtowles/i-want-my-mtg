edb5c47db721e5b4d8a0b4924908254f1efa7029
matthewdtowles <matthewdtowles@gmail.com>
2025-10-14 16:11:23 -0400

    sort options enum, query options update up and down arrow svgs query util functions

--- src/http/query/query-options.dto.ts
+++ src/http/query/query-options.dto.ts
@@ -1,15 +1,21 @@
+import { SortOptions } from "./sort-options.enum";
+
 export class QueryOptionsDto {
+    readonly DEFAULT_PAGE = 1;
+    readonly DEFAULT_LIMIT = 25;
+    readonly DEFAULT_ASCEND = false;
+
     readonly page: number;
     readonly limit: number;
-    readonly filter?: string;
-    readonly sort?: string;
     readonly ascend: boolean;
+    readonly filter?: string;
+    readonly sort?: SortOptions;
 
     constructor(init: Partial<QueryOptionsDto>) {
-        this.page = init.page ?? 1;
-        this.limit = init.limit ?? 25;
+        this.page = init.page ?? this.DEFAULT_PAGE;
+        this.limit = init.limit ?? this.DEFAULT_LIMIT;
+        this.ascend = init.ascend ?? this.DEFAULT_ASCEND;
         this.filter = init.filter;
-        this.sort = init.sort ?? "name";
-        this.ascend = init.ascend ?? false;
+        this.sort = init.sort;
     }
 }
---
+++ src/http/query/sort-options.enum.ts
@@ -0,0 +1,11 @@
+export enum SortOptions {
+    NAME = "name",
+    NUMBER = "number",
+    RELEASE_DATE = "releaseDate",
+    OWNED_QUANTITY = "quantity",
+    OWNED_VALUE = "ownedValue",
+    PRICE = "price",
+    PRICE_FOIL = "foilPrice",
+    SET = "set",
+    SET_VALUE = "setValue",
+}

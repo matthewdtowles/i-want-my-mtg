44fffec380a612947f3a04943e6fb0b450819807
matthewdtowles <matthewdtowles@gmail.com>
2025-11-02 20:12:27 -0500

    completion rate http util, specs

--- src/http/base/http.util.ts
+++ src/http/base/http.util.ts
@@ -26,3 +26,10 @@ export function buildQueryString(options: SafeQueryOptions): string {
     if (options.sort) params.push(`sort=${options.sort}`);
     return params.length > 0 ? `?${params.join("&")}` : "";
 }
+
+export function completionRate(totalOwned: number, totalCards: number): number {
+    if (totalCards <= 0 || totalOwned <= 0) return 0;
+    if (totalOwned >= totalCards) return 100;
+    const completionRate = (totalOwned / totalCards) * 100;
+    return Math.round(completionRate * 100) / 100;
+}
--- test/http/http.util.spec.ts
+++ test/http/http.util.spec.ts
@@ -1,4 +1,4 @@
-import { toDollar } from "src/http/base/http.util";
+import { completionRate, toDollar } from "src/http/base/http.util";
 
 describe("toDollar", () => {
     it("should handle numbers less than 1 correctly", () => {
@@ -32,5 +32,35 @@ describe("toDollar", () => {
 
     it("should handle small numbers correctly", () => {
         expect(toDollar(0.0001)).toBe("$0.01");
+    });
+});
+
+describe("completionRate", () => {
+    it("should return 0 if total owned is 0", () => {
+        expect(completionRate(0, 100)).toBe(0);
+    });
+
+    it("should return 0 if total owned is less than 0", () => {
+        expect(completionRate(-1, 100)).toBe(0);
+    });
+
+    it("should return 0 if total cards in set is 0", () => {
+        expect(completionRate(1, 0)).toBe(0);
+    });
+
+    it("should return 0 if total cards in set is less than 0", () => {
+        expect(completionRate(1, -1)).toBe(0);
+    });
+
+    it("should return 100 if total owned is equal to total in set", () => {
+        expect(completionRate(350, 350)).toBe(100);
+    });
+
+    it("should return 100 if total owned is greater than total in set", () => {
+        expect(completionRate(500, 350)).toBe(100);
+    });
+
+    it("should return completion rate to nearest two decimals", () => {
+        expect(completionRate(349, 350)).toBe(99.71);
     });
 });

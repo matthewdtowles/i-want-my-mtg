0ec61aad2f47dd77d27f311a2f6c35fdf5cbaef4
Matthew Towles <matthewdtowles@gmail.com>
2026-03-05 23:54:51 -0500

    Go to previous page upon login (#340)
    * Enhance login functionality with return URL support and improve error handling

--- src/http/auth/dto/login-form.view.dto.ts
+++ src/http/auth/dto/login-form.view.dto.ts
@@ -2,9 +2,11 @@ import { BaseViewDto } from 'src/http/base/base.view.dto';
 
 export class LoginFormViewDto extends BaseViewDto {
     readonly email: string;
+    readonly returnUrl: string;
 
     constructor(init: Partial<LoginFormViewDto> = {}) {
         super(init);
         this.email = init.email || '';
+        this.returnUrl = init.returnUrl || '';
     }
 }
